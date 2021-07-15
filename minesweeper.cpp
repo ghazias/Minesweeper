324a28400c5e8068619002b918992d3c49ebb3a4
aghazi <aghazi@protonmail.com>
2021-07-15 15:19:19 -0500

    implements various helper functions, small bug fixes, further TODOs notated

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,8 +1,11 @@
+#include <cstdlib>
 #include <iostream>
 #include <utility>
 #include <vector>
 
-#define REAL_GAME 0
+#define REAL_GAME 1
+
+// TODO write bomb_count_calculator function
 
 enum class GameResult { WIN, LOSE };
 enum class GameAction { FLAG, UNFLAG, REVEAL };
@@ -27,12 +30,33 @@ class Tile {
   bool is_flagged_{};
 };
 
+std::vector<std::pair<int, int>> generate_bomb_locations(
+    std::size_t width, std::size_t height, std::size_t total_bomb_count) {
+  std::vector<std::pair<int, int>> bomb_coordinates{};
+
+  for (std::size_t i = 0; i < total_bomb_count; ++i) {
+    bomb_coordinates.push_back({rand() % height, rand() % width});
+  }
+  // TODO write helper function which checks and replaces vector's duplicates
+  return bomb_coordinates;
+}
+
 class Board {
  public:
-  Board(int width, int height, int total_bomb_count)
-      : total_bomb_count_{total_bomb_count}, remaining_tiles_{width * height} {
-    // auto bomb_locations = generate_bomb_locations(width, height,
-    // total_bomb_count);
+  Board(const std::size_t width, const std::size_t height,
+        const std::size_t total_bomb_count)
+      : tiles_{height, std::vector<Tile>{width}},
+        total_bomb_count_{total_bomb_count},
+        remaining_tiles_{width * height} {
+    auto bomb_locations =
+        generate_bomb_locations(width, height, total_bomb_count);
+
+    for (const auto &[row, col] : bomb_locations) {
+      std::cout << row << ", " << col << "\n";
+      tiles_[row][col].set_is_bomb(true);
+    }
+
+    set_bomb_counts();
   }
 
   const std::vector<std::vector<Tile>> &tiles() const { return tiles_; };
@@ -40,43 +64,41 @@ class Board {
       tiles_;  // each vector in this vector is a row of tiles
                // with the entirety of the vector representing the board
 
-  bool is_bomb(int row, int col) const {
-    return tiles()[row - 1][col - 1].is_bomb();
+  void set_bomb_counts() {}  // TODO
+
+  bool is_bomb(std::size_t row, std::size_t col) const {
+    return tiles()[row][col].is_bomb();
   }
 
-  void flood(int row, int col) {
-    Tile &current = tiles_[row - 1][col - 1];
-    if (row == 0 || col == 0 || row == tiles().size() ||
+  void flood(std::size_t row, std::size_t col) {
+    if (row < 0 || col < 0 || row == tiles().size() ||
         col == tiles()[0].size()) {
-      if (current.bomb_count()) {
-        current.set_is_revealed(true);
-      }
-
       return;
     }
 
-    if (current.bomb_count()) {
+    Tile &current = tiles_[row][col];
+
+    if (!current.is_bomb()) {
       current.set_is_revealed(true);
+    }
+    if (!current.is_blank()) {
       return;
     }
 
-    current.set_is_revealed(true);
-
-    for (int i = row - 1; i < row + 1; ++i) {    // changed from size_t to int
-      for (int j = col - 1; j < col + 1; ++j) {  // to suppress warning
-        if (i == row && j == col) { // skip self
-          continue;
+    for (std::size_t i = row - 1; i < row + 1; ++i) {
+      for (std::size_t j = col - 1; j < col + 1; ++j) {
+        if (i != row || j != col) {
+          flood(i, j);
         }
-        flood(i, j);
       }
     }
   }
 
   int total_bomb_count() const { return total_bomb_count_; }
-  int remaining_tiles() const { return remaining_tiles_; }
+  std::size_t remaining_tiles() const { return remaining_tiles_; }
 
   void select(int row, int col, GameAction action) {
-    Tile &my_tile = tiles_[row - 1][col - 1];
+    Tile &my_tile = tiles_[row][col];
 
     switch (action) {
       case GameAction::FLAG:
@@ -86,45 +108,55 @@ class Board {
         my_tile.set_is_flagged(false);
         break;
       case GameAction::REVEAL:
-        my_tile.set_is_revealed(true);
-        // my_tile.flood(my_tile, row, col);
+        flood(row, col);
         break;
       default:
         break;
     }
   }
 
+  int width() const { return tiles()[0].size(); }
+  int height() const { return tiles().size(); }
+
   bool is_clear() const { return !(remaining_tiles() - total_bomb_count()); }
 
  private:
-  int total_bomb_count_{};  // TODO make const?
-  int remaining_tiles_{};
+  std::size_t total_bomb_count_{};
+  std::size_t remaining_tiles_{};
 };
 
-Tile create_bomb() {
-  Tile bomb_tile{};
-  bomb_tile.set_is_bomb(true);
-
-  return bomb_tile;
-}
-
-Board create_board(int width, int height, int total_bomb_count) {
+Board create_board(const std::size_t width, const std::size_t height,
+                   const std::size_t total_bomb_count) {
   return Board{width, height, total_bomb_count};
 }
 
 std::pair<int, int> get_user_choice(const Board &board) {
   std::pair<int, int> choice{};
-
-  std::cout << "Pick a coordinate to reveal.";
+  // add bounds checking through board reference
+  std::cout << "Pick a coordinate to reveal.\n";
   std::cout << "Enter row: ";
   std::cin >> choice.first;
+
   std::cout << "Enter col: ";
   std::cin >> choice.second;
 
   return choice;
 }
 
-GameAction get_user_action() { return GameAction::REVEAL; }  // TODO
+GameAction get_user_action() {
+  char choice{};
+  std::cout << "Enter action.\n";
+  std::cout << "(R)eveal, (F)lag, (U)nflag: ";
+  std::cin >> choice;
+
+  if (choice == 'R') {
+    return GameAction::REVEAL;
+  } else if (choice == 'F') {
+    return GameAction::FLAG;
+  } else {
+    return GameAction::UNFLAG;
+  }
+}
 
 std::string display_helper(const std::vector<Tile> &row) {}  // TODO
 
@@ -175,17 +207,25 @@ void game_over(GameResult result) {
 }
 
 int main(int, char *argv[]) {
-#if REAL_GAME
+  srand(time(NULL));
+  std::size_t width{}, height{}, total_bomb_count{};
   if (argv[1]) {
     switch (std::stoi(argv[1])) {
+      // create constexpr variables for these magic values?
       case 1:
-        create_board(9, 9, 10);
+        width = 9;
+        height = 9;
+        total_bomb_count = 10;
         break;
       case 2:
-        create_board(16, 16, 40);
+        width = 16;
+        height = 16;
+        total_bomb_count = 40;
         break;
       case 3:
-        create_board(30, 16, 99);
+        width = 30;
+        height = 16;
+        total_bomb_count = 99;
         break;
       default:
         std::cout << "Invalid choice\n";
@@ -196,7 +236,12 @@ int main(int, char *argv[]) {
     std::cout << "1 for easy, 2 for medium, 3 for hard\n";
     return 1;
   }
-#else
+
+  auto board = create_board(width, height, total_bomb_count);
+  auto result = play_game(board);
+  game_over(result);
+
+  /*
   auto board = create_board(0, 0, 0);
   board.tiles_.push_back({});
   auto &row = board.tiles_.back();
@@ -217,11 +262,5 @@ int main(int, char *argv[]) {
   std::cout << "-----------------\n";
   display(board);
   std::cout << "-----------------\n";
-#endif
-
-#if REAL_GAME
-  auto board = create_board(10, 15, 7);
-  auto result = play_game(board);
-  game_over(result);
-#endif
+  */
 }
